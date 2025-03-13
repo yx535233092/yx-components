93a1ccfd42ef13fc92dd00bb1f1fd87f54ac9a6a
杨潇 <535233092@qq.com>
2025-03-13 21:32:53 +0800

    change:docs base

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vitepress";
 export default defineConfig({
   title: "Yx Components",
   description: "Yx Components",
+  base: "/yx-components/",
   themeConfig: {
     nav: [
       {
