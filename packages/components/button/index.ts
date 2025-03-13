eb151a1096ebcb5783bc6189498380f955e31bf9
yx535233092 <yx535233092@users.noreply.github.com>
2025-03-13 13:26:48 +0000

    deploy: adfce9082b32d4929f7c349aef7928c979186407

--- packages/components/button/index.ts
+++
@@ -1,8 +0,0 @@
-import Button from "./Button.vue";
-import { App } from "vue";
-
-export default {
-  install(app: App) {
-    app.component("yx-button", Button);
-  },
-};
--- play/src/env.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="vite/client" />
-// play/src/env.d.ts
-declare module "*.vue" {
-  import type { DefineComponent } from "vue";
-  const component: DefineComponent<{}, {}, any>;
-  export default component;
-}
